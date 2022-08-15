95b41db6415c379d414bc3ffb7e6c31183dde675
Ruben Voss <bekertshop@gmail.com>
2022-08-15 12:01:05 +0200

    added bootstrap

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,15 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
   def answer
+    # makes @message the global variable for the user form input
+    @message = params['message']
+    # standard 'else' answer
+    @answer = "I don't care, get dressed and go to work!"
+    #  answer if the user wants to go to work
+    if @message == 'I am going to work' then @answer = 'Great!'
+    # answer if the user types in a question
+    elsif @message[-1] == '?' then @answer = 'Silly question, get dressed and go to work!' end
   end
 end
